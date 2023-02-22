e80d009efbd3d5a61ed1ed3aadc0902249ee93dc
tschmidt6 <tschmidt6@wisc.edu>
2023-02-22 13:34:01 -0600

    Initial Commit

---
+++ Fetch Challange/Fetch_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ChallangeApp.swift
+//  Fetch Challange
+//
+//  Created by Teryl S on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
